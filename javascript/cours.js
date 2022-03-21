5583e69f7fb3960b3f26d9d62ea54c4fa324a326
Kadiatou <kadidans@gmail.com>
2022-03-21 14:31:27 +0100

    premier commentaire

---
+++ javascript/cours.js
@@ -0,0 +1,40 @@
+//Tableau et objet
+let array= ["je", "suis", "audrey"];
+let objet = {
+    nom: "manon",
+    age: 25,
+    ville: "lille"
+};
+console.log(objet);
+//Arithmétique avec la valeur des variables
+let total =2;
+let x =4;
+total++;
+total++; 
+total++;
+console.log(total);
+
+total +=10;
+console.log(total);
+
+total = ++x;
+console.log(total);
+
+// conditions
+let y = 0
+let z = 2
+
+if (y<z) {
+    document.write("y est inferieur à z");
+}
+else{
+    document.write("y est superieur à z");
+};
+// les conditions et comparateur
+if(y==z || y<3){
+    console.log("True !");
+}
+else{
+    console.log("false !");
+}
+
